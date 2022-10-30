1426290e9408640ced3e798a0fbe48703e80b069
Adam Agerling <alltidvakenhehe@gmail.com>
2022-10-30 17:19:07 +0100

    testing

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,46 +1,49 @@
-import { ButtonHTMLAttributes, FC, ReactNode } from "react";
-import styled, {css} from "styled-components";
-
-
+import { ButtonHTMLAttributes, FC, ReactNode } from 'react';
+import styled, { css } from 'styled-components';
 
 type ButtonProps = {
-children?: ReactNode;
-colorVariant?: 'primary' | 'secondary' | 'tretiary';
+  children?: ReactNode;
+  colorVariant?: 'primary' | 'secondary' | 'tretiary';
 } & ButtonHTMLAttributes<HTMLButtonElement>;
 
 const StyledButton = styled.button<ButtonProps>`
-cursor: pointer;
-color: ${({theme}) => theme.colors.textPrimary};
-background-color: ${({theme}) => theme.colors.primary};
-border-radius: 5px;
-padding: 5px 9px;
-border-color: transparent;
-&:disabled{
-  background-color: ${({theme}) => theme.colors.disabled};
-}
-&:active {
-  border: 2px solid ${({theme}) => theme.colors.secondary};
-}
+  cursor: pointer;
+  color: ${({ theme }) => theme.colors.textPrimary};
+  background-color: ${({ theme }) => theme.colors.primary};
+  border-radius: 5px;
+  padding: 5px 9px;
+  border-color: transparent;
+  &:disabled {
+    background-color: ${({ theme }) => theme.colors.disabled};
+  }
+  &:active {
+    border: 2px solid ${({ theme }) => theme.colors.secondary};
+  }
 
-${({colorVariant}) => colorVariant === 'secondary' && css`
-background-color: #5c0080;
-color: ${({theme}) => theme.colors.textSecondary};
-`
-}
-${({colorVariant}) => colorVariant === 'tretiary' && css`
-background-color: transparent;
-&:hover{
-  background-color: ${({theme}) => theme.colors.primary};
-}
-`}
+  ${({ colorVariant }) =>
+    colorVariant === 'secondary' &&
+    css`
+      background-color: #5c0080;
+      color: ${({ theme }) => theme.colors.textSecondary};
+    `}
+  ${({ colorVariant }) =>
+    colorVariant === 'tretiary' &&
+    css`
+      background-color: transparent;
+      &:hover {
+        background-color: ${({ theme }) => theme.colors.primary};
+      }
+    `}
+`;
 
-`
-
-
-export const Button: FC<ButtonProps> =({children, colorVariant = 'primary', ...props}) => {
+export const Button: FC<ButtonProps> = ({
+  children,
+  colorVariant = 'primary',
+  ...props
+}) => {
   return (
     <StyledButton {...props} colorVariant={colorVariant}>
       {children}
     </StyledButton>
-  )
-}
+  );
+};
